1b6f45df5cad7366602ceeda911ae499d6a75c93
Mehdi-MMO <mesfrer135@gmail.com>
2023-02-16 16:44:57 +0100

    Entity Generators Optimizations

--- client/EntityGenerators.lua
+++ client/EntityGenerators.lua
@@ -1,18 +1,13 @@
-if Config.ManagedDensity then
-    Citizen.CreateThread(function()
-        while true do
-            Citizen.Wait(0)
-            SetVehicleDensityMultiplierThisFrame(Config.GeneratorsDensity['vehicle'])
-            SetParkedVehicleDensityMultiplierThisFrame(Config.GeneratorsDensity['parked'])
-            SetRandomVehicleDensityMultiplierThisFrame(Config.GeneratorsDensity['multiplier'])
-            SetPedDensityMultiplierThisFrame(Config.GeneratorsDensity['peds'])
-            SetScenarioPedDensityMultiplierThisFrame(Config.GeneratorsDensity['actions'], Config.GeneratorsDensity['actions'])
-        end
-    end)
-elseif Config.TrafficZones then
-    Citizen.CreateThread(function()
-        while true do
-            Citizen.Wait(1000)
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(Config.ManagedDensity and 0 or 1000)
+        if Config.ManagedDensity then
+            SetVehicleDensityMultiplierThisFrame(Config.GeneratorsDensity.vehicle)
+            SetParkedVehicleDensityMultiplierThisFrame(Config.GeneratorsDensity.parked)
+            SetRandomVehicleDensityMultiplierThisFrame(Config.GeneratorsDensity.multiplier)
+            SetPedDensityMultiplierThisFrame(Config.GeneratorsDensity.peds)
+            SetScenarioPedDensityMultiplierThisFrame(Config.GeneratorsDensity.actions, Config.GeneratorsDensity.actions)
+        elseif Config.TrafficZones then
             for i = 1, #Config.TrafficArea do
                 ClearAreaOfPeds(Config.TrafficArea[i].pos.x, Config.TrafficArea[i].pos.y, Config.TrafficArea[i].pos.z,
                     Config.TrafficArea[i].radius, false, false, false, false, false)
@@ -20,5 +15,5 @@ elseif Config.TrafficZones then
                     Config.TrafficArea[i].pos.z, Config.TrafficArea[i].radius, false, false, false, false, false)
             end
         end
-    end)
-end
+    end
+end)
