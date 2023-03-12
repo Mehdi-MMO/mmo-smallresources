a38a3b7455dd5897b44b7bf84c4958c3c6d345ab
Mehdi-MMO <mesfrer135@gmail.com>
2023-03-12 22:02:37 +0100

    bug fix 2.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 title "mmo-smallresources"
 author "Mehdi MMO - Sweet Vibes#7561"
 description "A script that contains multiple needed roleplay functionalities"
-version "1.1.2"
+version "1.1.6"
 
 -- Discord http://discord.gg/FqQFzndxZ4
 
