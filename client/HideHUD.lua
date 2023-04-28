028e8c3582745e62019ac59013179bcc267ec5ec
Mehdi <57235093+Mehdi-MMO@users.noreply.github.com>
2023-04-28 23:50:20 +0100

    Update HideHUD.lua

--- client/HideHUD.lua
+++ client/HideHUD.lua
@@ -8,3 +8,22 @@ if Config.HideHud then
         end
     end)
 end
+
+if Config.weaponReticle then
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(0)
+
+            local ped = GetPlayerPed(-1)
+            local currentWeaponHash = GetSelectedPedWeapon(ped)
+            local isSniper = (currentWeaponHash == 100416529 or
+                              currentWeaponHash == 205991906 or
+                              currentWeaponHash == -952879014 or
+                              currentWeaponHash == GetHashKey('WEAPON_HEAVYSNIPER_MK2'))
+
+            if not isSniper then
+                HideHudComponentThisFrame(14)
+            end
+        end
+    end)
+end
